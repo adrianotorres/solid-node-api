4611f53124c306982392d2b61c52a37352cb1f2e
Adriano Torres <adriano.skilo@gmail.com>
2023-11-15 23:04:59 -0300

    init use case factories

--- src/http/controllers/authenticate.controller.ts
+++ src/http/controllers/authenticate.controller.ts
@@ -1,6 +1,5 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users.repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -15,8 +14,7 @@ export async function authenticate(
 
   const { email, password } = registerSchema.parse(request.body)
 
-  const prismaUsersRepository = new PrismaUsersRepository()
-  const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository)
+  const authenticateUseCase = makeAuthenticateUseCase()
 
   try {
     await authenticateUseCase.execute({ email, password })
